e090b7cd0d0f874a79da27e0dcb2255bd8c267f4
Gabriel <rizangabriel3@gmail.com>
2024-07-30 19:37:20 +0300

    Routing, queryparams, router params, routerlinkactive

--- src/app/catalog/catalog.component.ts
+++ src/app/catalog/catalog.component.ts
@@ -2,6 +2,7 @@ import { Component, inject } from '@angular/core';
 import { IProduct } from './product.model';
 import { CartService } from '../cart/cart.service';
 import { ProductService } from './product.service';
+import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
   selector: 'bot-catalog',
@@ -14,24 +15,30 @@ export class CatalogComponent {
 
   constructor(
     private cartSvc: CartService,
-    private productSvc: ProductService
-  ) { }
+    private productSvc: ProductService,
+    private router: Router,
+    private route: ActivatedRoute
+  ) {}
 
   ngOnInit() {
     this.productSvc.getProducts().subscribe((products) => {
       this.products = products;
     });
+    this.route.queryParams.subscribe((params) => {
+      this.filter = params['filter'] || '';
+    });
   }
 
   addToCart(product: IProduct) {
     this.cartSvc.add(product);
+    this.router.navigate(['/cart']);
   }
 
   getFilteredProducts() {
     return this.filter === ''
       ? this.products
       : this.products.filter(
-        (product: any) => product.category === this.filter
-      );
+          (product: any) => product.category === this.filter
+        );
   }
 }
